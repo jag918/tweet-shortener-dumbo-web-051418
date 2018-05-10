59832b3b40ff02a35f7136732c96a7286225ea2e
jee <jag918@nyu.edu>
2018-05-10 05:21:51 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -18,6 +18,6 @@ def word_substituter(tweet)
   dictionary_words = dictionary
   words = tweet.split()
   words.each_with_index do |word,index|
-    if dictionary_words.has_key? 
+    if dictionary_words.has_key? word
   end
 end
