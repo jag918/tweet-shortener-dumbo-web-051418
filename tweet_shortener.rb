4ba494a73450b72dda4de7323dc140a8aa923810
jee <jag918@nyu.edu>
2018-05-10 05:17:42 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+
+def dictionary
+  words = {
+    "hello":"hi"
+    "to":"2",
+    "two":"2".
+    "too":"2",
+    "for":"4",
+    "be":"b",
+    "you":"u",
+    "at":"@",
+    "and":"&"
+  }
+end
