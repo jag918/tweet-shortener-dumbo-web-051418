5a21b97c1fc5b1958ba8249dd5c59311e310901b
jee <jag918@nyu.edu>
2018-05-10 05:29:40 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -31,3 +31,11 @@ def bulk_tweet_shortener(tweets)
     puts(word_substituter(tweet))
   end
 end
+
+def selective_tweet_shortener(tweet)
+  final_tweet = tweet
+  if tweet.length > 140
+    word_substituter(tweet)
+  end
+  final_tweet
+end
